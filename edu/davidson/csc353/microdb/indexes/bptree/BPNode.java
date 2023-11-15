eff3660bf1576ac346c55675326cb3d3e7868d5e
Anh Hoang <anhhoang14022001@gmail.com>
2023-11-14 20:15:49 -0500

    Implement get() & find() and reformat the code

--- edu/davidson/csc353/microdb/indexes/bptree/BPNode.java
+++ edu/davidson/csc353/microdb/indexes/bptree/BPNode.java
@@ -1,269 +1,265 @@
 package edu.davidson.csc353.microdb.indexes.bptree;
 
 import java.nio.ByteBuffer;
-
 import java.util.ArrayList;
-
 import java.util.function.Function;
 
 /**
  * B+Tree node (internal or leaf).
- * 
+ *
  * @param <K> Type of the keys in the B+Tree node.
  * @param <V> type of the values associated with the keys in the B+Tree node.
  */
 public class BPNode<K extends Comparable<K>, V> {
-	// Maximum number of children in an internal node (fanout)
-	// A leaf node can have at most SIZE - 1 key-value pairs
-	public static int SIZE = 5;
+    // Maximum number of children in an internal node (fanout)
+    // A leaf node can have at most SIZE - 1 key-value pairs
+    public static int SIZE = 5;
+    // Reference to the parent
+    public int parent;
+    // Keys:
+    // -  in the leaf node, they are the associated with values
+    // -  in internal nodes, they separate the pointers to the children
+    public ArrayList<K> keys;
+    // (Leaf node only) Values associated with each key
+    public ArrayList<V> values;
+    // (Leaf node only) Next leaf node
+    public int next;
+    // (Internal node only) Children of the internal node
+    public ArrayList<Integer> children;
+    // Node number
+    public int number;
+    // Flag indicating if a leaf or not
+    boolean leaf;
 
-	// Flag indicating if a leaf or not
-	boolean leaf;
+    /**
+     * Constructor.
+     *
+     * @param leaf Flag indicating whether the node is a leaf (true) or internal node (false)
+     */
+    public BPNode(boolean leaf) {
+        this.leaf = leaf;
 
-	// Reference to the parent
-	public int parent;
+        this.parent = -1;
+        this.next = -1;
 
-	// Keys:
-	// -  in the leaf node, they are the associated with values
-	// -  in internal nodes, they separate the pointers to the children
-	public ArrayList<K> keys;
+        // Contains one more than you allow, because you may want to add, and then split
+        this.keys = new ArrayList<>(SIZE);
 
-	// (Leaf node only) Values associated with each key
-	public ArrayList<V> values;
-	// (Leaf node only) Next leaf node
-	public int next;
+        // Contains one more than you allow, because you may want to add, and then split
+        this.values = new ArrayList<>(SIZE);
 
-	// (Internal node only) Children of the internal node
-	public ArrayList<Integer> children; 
+        // Contains one more than you allow, because you may want to add, and then split
+        this.children = new ArrayList<>(SIZE + 1);
+    }
 
-	// Node number
-	public int number;
+    /**
+     * Returns true if the B+Tree node is a leaf node.
+     *
+     * @return True if the B+Tree node is a leaf node.
+     */
+    public boolean isLeaf() {
+        return leaf;
+    }
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param leaf Flag indicating whether the node is a leaf (true) or internal node (false)
-	 */
-	public BPNode(boolean leaf) {
-		this.leaf = leaf;
+    /**
+     * Returns the key in a given position.
+     *
+     * @param index Position of the key.
+     * @return Key in the specified position.
+     */
+    public K getKey(int index) {
+        return keys.get(index);
+    }
 
-		this.parent = -1;
-		this.next = -1;
+    /**
+     * Returns the child in a given position.
+     * This can only be called for internal nodes.
+     *
+     * @param index Position of the child.
+     * @return Child in the specified position.
+     */
+    public int getChild(int index) {
+        return children.get(index);
+    }
 
-		// Contains one more than you allow, because you may want to add, and then split
-		this.keys = new ArrayList<>(SIZE);
+    /**
+     * Returns the value in a given position.
+     * This can only be called for leaf nodes.
+     *
+     * @param index Position of the value.
+     * @return Value in the specified position.
+     */
+    public V getValue(int index) {
+        return values.get(index);
+    }
 
-		// Contains one more than you allow, because you may want to add, and then split
-		this.values = new ArrayList<>(SIZE);
+    /**
+     * Returns the number of keys in this node.
+     *
+     * @return The number of keys.
+     */
+    public int getNumberOfKeys() {
+        return keys.size();
+    }
 
-		// Contains one more than you allow, because you may want to add, and then split
-		this.children = new ArrayList<>(SIZE + 1);
-	}
+    /**
+     * (Leaf node only) Inserts a (K,P) pair in the appropriate position in the node.
+     *
+     * @param key   The key.
+     * @param value The value associated with the key.
+     */
+    public void insertValue(K key, V value) {
+        int i;
 
-	/**
-	 * Returns true if the B+Tree node is a leaf node.
-	 * 
-	 * @return True if the B+Tree node is a leaf node.
-	 */
-	public boolean isLeaf() {
-		return leaf;
-	}
+        for (i = 0; i < keys.size(); i++) {
+            if (BPTree.more(keys.get(i), key)) {
+                break;
+            }
+        }
 
-	/**
-	 * Returns the key in a given position.
-	 * 
-	 * @param index Position of the key.
-	 * 
-	 * @return Key in the specified position.
-	 */
-	public K getKey(int index) {
-		return keys.get(index);
-	}
+        keys.add(i, key);
+        values.add(i, value);
+    }
 
-	/**
-	 * Returns the child in a given position.
-	 * This can only be called for internal nodes.
-	 * 
-	 * @param index Position of the child.
-	 * 
-	 * @return Child in the specified position.
-	 */
-	public int getChild(int index) {
-		return children.get(index);
-	}
+    /**
+     * (Internal node only) Inserts a (K,P) pair in the appropriate position in the node.
+     * <p>
+     * NOTE: The pointer associated with key at position i is actually located at position i+1
+     * due to the first pointer.
+     *
+     * @param key         The key.
+     * @param value       The child node reference that should FOLLOW the key.
+     * @param nodeFactory Factory that generates new nodes.
+     */
+    public void insertChild(K key, int childNumber, BPNodeFactory<K, V> nodeFactory) {
+        int i;
 
-	/**
-	 * Returns the value in a given position.
-	 * This can only be called for leaf nodes.
-	 * 
-	 * @param index Position of the value.
-	 * 
-	 * @return Value in the specified position.
-	 */
-	public V getValue(int index) {
-		return values.get(index);
-	}
+        for (i = 0; i < keys.size(); i++) {
+            if (BPTree.more(keys.get(i), key)) {
+                break;
+            }
+        }
 
-	/**
-	 * (Leaf node only) Inserts a (K,P) pair in the appropriate position in the node.
-	 * 
-	 * @param key The key.
-	 * @param value The value associated with the key.
-	 */
-	public void insertValue(K key, V value) {
-		int i;
+        keys.add(i, key);
+        children.add(i + 1, childNumber);
 
-		for(i = 0; i < keys.size(); i++) {
-			if(BPTree.more(keys.get(i), key)) {
-				break;
-			}
-		}
+        BPNode<K, V> child = nodeFactory.getNode(childNumber);
 
-		keys.add(i, key);
-		values.add(i, value);
-	}
+        child.parent = this.number;
+        //nodeFactory.save(child);
+    }
 
-	/** 
-	 * (Internal node only) Inserts a (K,P) pair in the appropriate position in the node.
-	 * 
-	 * NOTE: The pointer associated with key at position i is actually located at position i+1
-	 *       due to the first pointer.
-	 * 
-	 * @param key The key.
-	 * @param value The child node reference that should FOLLOW the key.
-	 * @param nodeFactory Factory that generates new nodes.
-	 */
-	public void insertChild(K key, int childNumber, BPNodeFactory<K,V> nodeFactory) {
-		int i;
+    /**
+     * Splits an overflowed leaf node into a {@link SplitResult} object,
+     * that contains:
+     * - A reference to the left B+Tree node;
+     * - A reference to the right B+Tree node;
+     * - The first key in the right B+Tree node (the "divider Key" in the {@link SplitResult} object).
+     * <p>
+     * The method also makes the left node point to the right node in the "next" attribute.
+     *
+     * @param nodeFactory Factory that generates new nodes.
+     * @return A new {@link SplitResult} following the specifications above.
+     */
+    public SplitResult<K, V> splitLeaf(BPNodeFactory<K, V> nodeFactory) {
+        SplitResult<K, V> result = new SplitResult<>();
 
-		for(i = 0; i < keys.size(); i++) {
-			if(BPTree.more(keys.get(i), key)) {
-				break;
-			}
-		}
+        result.left = this;
+        result.right = nodeFactory.create(true);
 
-		keys.add(i, key);
-		children.add(i + 1, childNumber);
+        // TODO ...
 
-		BPNode<K,V> child = nodeFactory.getNode(childNumber);
-		
-		child.parent = this.number;
-		//nodeFactory.save(child);
-	}
+        return result;
+    }
 
-	/**
-	 * Splits an overflowed leaf node into a {@link SplitResult} object,
-	 * that contains:
-	 * 	- A reference to the left B+Tree node;
-	 * 	- A reference to the right B+Tree node;
-	 * 	- The first key in the right B+Tree node (the "divider Key" in the {@link SplitResult} object).
-	 * 
-	 * The method also makes the left node point to the right node in the "next" attribute.
-	 * 
-	 * @param nodeFactory Factory that generates new nodes.
-	 * 
-	 * @return A new {@link SplitResult} following the specifications above.
-	 */
-	public SplitResult<K,V> splitLeaf(BPNodeFactory<K,V> nodeFactory) {
-		SplitResult<K,V> result = new SplitResult<>();
+    /**
+     * Splits an overflowed leaf internal into a {@link SplitResult} object,
+     * that contains:
+     * - A reference to the left B+Tree node;
+     * - A reference to the right B+Tree node;
+     * - The divider key that will be subsequently be inserted in the parent node
+     * (stored in the "divider Key" in the {@link SplitResult} object).
+     *
+     * @param nodeFactory Factory that generates new nodes.
+     * @return A new {@link SplitResult} following the specifications above.
+     */
+    public SplitResult<K, V> splitInternal(BPNodeFactory<K, V> nodeFactory) {
+        SplitResult<K, V> result = new SplitResult<>();
 
-		result.left = this;
-		result.right = nodeFactory.create(true);
+        result.left = this;
+        result.right = nodeFactory.create(false);
 
-		// TODO ...
+        // TODO ...
 
-		return result;
-	}
+        return result;
+    }
 
-	/**
-	 * Splits an overflowed leaf internal into a {@link SplitResult} object,
-	 * that contains:
-	 * 	- A reference to the left B+Tree node;
-	 * 	- A reference to the right B+Tree node;
-	 * 	- The divider key that will be subsequently be inserted in the parent node
-	 *    (stored in the "divider Key" in the {@link SplitResult} object).
-	 *    
-	 * @param nodeFactory Factory that generates new nodes.
-	 * 
-	 * @return A new {@link SplitResult} following the specifications above.
-	 */
-	public SplitResult<K, V> splitInternal(BPNodeFactory<K,V> nodeFactory) {
-		SplitResult<K,V> result = new SplitResult<>();
+    /**
+     * Returns a string representation of the B+Tree node.
+     */
+    public String toString() {
+        String result = "[(#: " + number + ") ";
 
-		result.left = this;
-		result.right = nodeFactory.create(false);
+        // If leaf, print [k1 v1 k2 v2 ... kn vn]
+        if (isLeaf()) {
+            for (int i = 0; i < keys.size() - 1; i++) {
+                result += keys.get(i);
+                result += " ";
+                result += values.get(i);
+                result += " ";
+            }
 
-		// TODO ...
+            if (keys.size() > 0) {
+                result += keys.get(keys.size() - 1);
+                result += " ";
+                result += values.get(keys.size() - 1);
+            }
+        }
+        // If internal, print [<c0> k1 <c1> k2 <c2> ... kn <cn>], where
+        else {
+            for (int i = 0; i < keys.size(); i++) {
+                result += "<" + children.get(i) + ">";
+                result += " ";
+                result += keys.get(i);
+                result += " ";
+            }
 
-		return result;
-	}
+            if (keys.size() > 0) {
+                result += "<" + children.get(keys.size()) + ">";
+            }
+        }
 
-	/**
-	 * Returns a string representation of the B+Tree node.
-	 */
-	public String toString() {
-		String result = "[(#: " + number + ") ";
+        result += "]";
 
-		// If leaf, print [k1 v1 k2 v2 ... kn vn]
-		if(isLeaf()) {
-			for(int i = 0; i < keys.size() - 1; i++) {
-				result += keys.get(i);
-				result += " ";
-				result += values.get(i);
-				result += " ";
-			}
+        return result;
+    }
 
-			if(keys.size() > 0) {
-				result += keys.get(keys.size() - 1);
-				result += " ";
-				result += values.get(keys.size() - 1);
-			}
-		}
-		// If internal, print [<c0> k1 <c1> k2 <c2> ... kn <cn>], where
-		else {
-			for(int i = 0; i < keys.size(); i++) {
-				result += "<" + children.get(i) + ">";
-				result += " ";
-				result += keys.get(i);
-				result += " ";
-			}
+    /**
+     * Loads information from a buffer of 512 bytes (loaded from disk)
+     * into the memory representation of a B+Tree node.
+     *
+     * @param buffer    A byte buffer of 512 bytes containing the node representation from disk.
+     * @param loadKey   A function that converts string representations into keys of type K
+     * @param loadValue A function that converts string representations into values of type V
+     */
+    public void load(ByteBuffer buffer, Function<String, K> loadKey, Function<String, V> loadValue) {
+        buffer.rewind();
 
-			if(keys.size() > 0) {
-				result += "<" + children.get(keys.size()) + ">";
-			}
-		}
+        // TODO: Load from disk (that is, from the buffer), create your own file format
+        //       The getInt() and putInt() functions should be very helpful
+    }
 
-		result += "]";
+    /**
+     * Save the memory representation of the B+Tree node
+     * into a buffer of 512 bytes (to be stored on disk).
+     *
+     * @param buffer
+     */
+    public void save(ByteBuffer buffer) {
+        buffer.rewind();
 
-		return result;
-	}
-
-	/**
-	 * Loads information from a buffer of 512 bytes (loaded from disk)
-	 * into the memory representation of a B+Tree node.
-	 * 
-	 * @param buffer A byte buffer of 512 bytes containing the node representation from disk.
-	 * @param loadKey A function that converts string representations into keys of type K
-	 * @param loadValue A function that converts string representations into values of type V
-	 */
-	public void load(ByteBuffer buffer, Function<String, K> loadKey, Function<String, V> loadValue) {
-		buffer.rewind();
-
-		// TODO: Load from disk (that is, from the buffer), create your own file format
-		//       The getInt() and putInt() functions should be very helpful
-	}
-
-	/**
-	 * Save the memory representation of the B+Tree node
-	 * into a buffer of 512 bytes (to be stored on disk).
-	 * 
-	 * @param buffer
-	 */
-	public void save(ByteBuffer buffer) {
-		buffer.rewind();
-
-		// TODO: Save to disk (that is, to the buffer), create your own file format
-		//       The getInt() and putInt() functions should be very helpful
-		//       To save a string, generate it in memory, then use getBytes() and use the put() function in the buffer.
-	}
+        // TODO: Save to disk (that is, to the buffer), create your own file format
+        //       The getInt() and putInt() functions should be very helpful
+        //       To save a string, generate it in memory, then use getBytes() and use the put() function in the buffer.
+    }
 }
--- edu/davidson/csc353/microdb/indexes/bptree/BPTree.java
+++ edu/davidson/csc353/microdb/indexes/bptree/BPTree.java
@@ -1,7 +1,6 @@
 package edu.davidson.csc353.microdb.indexes.bptree;
 
 import java.nio.ByteBuffer;
-
 import java.util.function.Function;
 
 /**
@@ -11,164 +10,175 @@ import java.util.function.Function;
  * @param <V> type of the values associated with the keys in the B+Tree node.
  */
 public class BPTree<K extends Comparable<K>, V> {
-	private BPNodeFactory<K,V> nodeFactory;
+    private final BPNodeFactory<K, V> nodeFactory;
 
-	private int rootNumber;
+    private final int rootNumber;
 
-	public BPTree(Function<String, K> loadKey, Function<String, V> loadValue) {
-		nodeFactory = new BPNodeFactory<>("test-index", loadKey, loadValue);
+    public BPTree(Function<String, K> loadKey, Function<String, V> loadValue) {
+        nodeFactory = new BPNodeFactory<>("test-index", loadKey, loadValue);
 
-		// The node will be garbage-collected, but retrievable from disk
-		BPNode<K,V> rootNode = nodeFactory.create(true);
-		//nodeFactory.save(rootNode);
+        // The node will be garbage-collected, but retrievable from disk
+        BPNode<K, V> rootNode = nodeFactory.create(true);
+        //nodeFactory.save(rootNode);
 
-		rootNumber = rootNode.number;
-	}
+        rootNumber = rootNode.number;
+    }
 
-	/**
-	 * Inserts the key-value pair into the B+Tree.
-	 * 
-	 * @param key The key.
-	 * @param value The value associated with the key.
-	 */
-	public void insert(K key, V value) {
-		System.out.println("Inserting " + key);
+    /**
+     * Helper method: returns true if k1 < k2.
+     *
+     * @param k1 The first key k1.
+     * @param k2 The second key k2.
+     * @return True iff k1 < k2.
+     */
+    public static <K extends Comparable<K>> boolean less(K k1, K k2) {
+        return k1.compareTo(k2) < 0;
+    }
 
-		BPNode<K,V> insertPlace = find(nodeFactory.getNode(rootNumber), key);
+    /**
+     * Helper method: returns true if k1 == k2.
+     *
+     * @param k1 The first key k1.
+     * @param k2 The second key k2.
+     * @return True iff k1 == k2.
+     */
+    public static <K extends Comparable<K>> boolean equal(K k1, K k2) {
+        return k1.compareTo(k2) == 0;
+    }
 
-		// TODO ...
-		
-		// Need to call insertOnParent after performing a leaf node split
-	}
+    /**
+     * Helper method: returns true if k1 > k2.
+     *
+     * @param k1 The first key k1.
+     * @param k2 The second key k2.
+     * @return True iff k1 > k2.
+     */
+    public static <K extends Comparable<K>> boolean more(K k1, K k2) {
+        return k1.compareTo(k2) > 0;
+    }
 
-	/**
-	 * Insert on parent node a divider key after a child has been splitted.
-	 * 
-	 * @param left Left B+Tree node after a split has been made.
-	 * @param key Divider key (according to the description in {@link SplitResult}.
-	 * @param right Right B+Tree node after a split has been made.
-	 */
-	private void insertOnParent(BPNode<K,V> left, K key, BPNode<K,V> right) {
-		// TODO ...
-					
-		// Need to keep calling insertOnParent after performing an internal node split
-	}
+    /**
+     * Main method. Creates a test index and add some key-value pairs.
+     *
+     * @param arguments None.
+     */
+    public static void main(String[] arguments) {
+        BPTree<String, Integer> testIndex = new BPTree<>(k -> k, s -> Integer.parseInt(s));
 
-	/**
-	 * Returns a value associated with a particular key.
-	 * 
-	 * @param key The key.
-	 * 
-	 * @return The value associated with the provided key.
-	 */
-	public V get(K key) {
-		// TODO ...
-		return null;
-	}
+        testIndex.insert("i", 9);
+        testIndex.insert("l", 12);
+        testIndex.insert("g", 7);
+        testIndex.insert("o", 15);
+        testIndex.insert("d", 4); // Generates leaf node split
+        testIndex.insert("f", 6);
+        testIndex.insert("a", 1);
+        testIndex.insert("c", 3); // Generates leaf node split
+        testIndex.insert("p", 16);
+        testIndex.insert("k", 11); // Generates leaf node split
+        testIndex.insert("b", 2);
+        testIndex.insert("e", 5);
+        testIndex.insert("n", 14);
+        testIndex.insert("h", 8); // Generates leaf node split
+        testIndex.insert("j", 10);
+        testIndex.insert("m", 13); // Generates an internal node split
 
-	/**
-	 * Returns the leaf node where we should look for the provided key.
-	 * 
-	 * @param node The current node in the recursive search procedure.
-	 * @param key The key being searched.
-	 * 
-	 * @return The leaf node where we should look for the provided key.
-	 */
-	private BPNode<K,V> find(BPNode<K,V> node, K key) {
-		if(node.isLeaf()) {
-			return node;
-		}
+        System.out.println(testIndex.get("g")); // Should print 7
+        System.out.println(testIndex.get("o")); // Should print 15
+        System.out.println(testIndex.get("a")); // Should print 1
+        System.out.println(testIndex.get("c")); // Should print 3
+        System.out.println(testIndex.get("x")); // Should print null
 
-		// TODO ...
-		return null;
-	}
+        BPNode<String, Integer> node1 = testIndex.nodeFactory.getNode(testIndex.rootNumber);
+        BPNode<String, Integer> node2 = testIndex.find(node1, "a");
 
-	/**
-	 * Helper method: returns true if k1 < k2.
-	 * 
-	 * @param k1 The first key k1.
-	 * @param k2 The second key k2.
-	 * 
-	 * @return True iff k1 < k2.
-	 */
-	public static <K extends Comparable<K>> boolean less(K k1, K k2) {
-		return k1.compareTo(k2) < 0;
-	}
+        ByteBuffer buffer1 = ByteBuffer.allocate(512);
+        ByteBuffer buffer2 = ByteBuffer.allocate(512);
 
-	/**
-	 * Helper method: returns true if k1 == k2.
-	 * 
-	 * @param k1 The first key k1.
-	 * @param k2 The second key k2.
-	 * 
-	 * @return True iff k1 == k2.
-	 */
-	public static <K extends Comparable<K>> boolean equal(K k1, K k2) {
-		return k1.compareTo(k2) == 0;
-	}
+        node1.save(buffer1);
+        node2.save(buffer2);
 
-	/**
-	 * Helper method: returns true if k1 > k2.
-	 * 
-	 * @param k1 The first key k1.
-	 * @param k2 The second key k2.
-	 * 
-	 * @return True iff k1 > k2.
-	 */
-	public static <K extends Comparable<K>> boolean more(K k1, K k2) {
-		return k1.compareTo(k2) > 0;
-	}
+        BPNode<String, Integer> newNode1 = new BPNode<>(false);
+        BPNode<String, Integer> newNode2 = new BPNode<>(true);
 
-	/**
-	 * Main method. Creates a test index and add some key-value pairs.
-	 * 
-	 * @param arguments None.
-	 */
-	public static void main(String[] arguments) {
-		BPTree<String, Integer> testIndex = new BPTree<>(k -> k, s -> Integer.parseInt(s));
+        newNode1.load(buffer1, k -> k, s -> Integer.parseInt(s));
+        newNode2.load(buffer2, k -> k, s -> Integer.parseInt(s));
 
-		testIndex.insert("i", 9);
-		testIndex.insert("l", 12);
-		testIndex.insert("g", 7);
-		testIndex.insert("o", 15);
-		testIndex.insert("d", 4); // Generates leaf node split
-		testIndex.insert("f", 6);
-		testIndex.insert("a", 1);
-		testIndex.insert("c", 3); // Generates leaf node split
-		testIndex.insert("p", 16);
-		testIndex.insert("k", 11); // Generates leaf node split
-		testIndex.insert("b", 2);
-		testIndex.insert("e", 5);
-		testIndex.insert("n", 14);
-		testIndex.insert("h", 8); // Generates leaf node split
-		testIndex.insert("j", 10);
-		testIndex.insert("m", 13); // Generates an internal node split
+        System.out.println("Original root: " + node1 + ", parent = " + node1.parent + ", next = " + node1.next + ", number = " + newNode1.number);
+        System.out.println("Loaded root:   " + newNode1 + ", parent = " + newNode1.parent + ", next = " + newNode1.next + ", number = " + newNode1.number);
 
-		System.out.println(testIndex.get("g")); // Should print 7
-		System.out.println(testIndex.get("o")); // Should print 15
-		System.out.println(testIndex.get("a")); // Should print 1
-		System.out.println(testIndex.get("c")); // Should print 3
-		System.out.println(testIndex.get("x")); // Should print null
+        System.out.println("Original leaf: " + node2 + ", parent = " + node2.parent + ", next = " + node2.next + ", number = " + newNode2.number);
+        System.out.println("Loaded leaf:   " + newNode2 + ", parent = " + newNode2.parent + ", next = " + newNode2.next + ", number = " + newNode2.number);
+    }
 
-		BPNode<String, Integer> node1 = testIndex.nodeFactory.getNode(testIndex.rootNumber);
-		BPNode<String, Integer> node2 = testIndex.find(node1, "a");
+    /**
+     * Inserts the key-value pair into the B+Tree.
+     *
+     * @param key   The key.
+     * @param value The value associated with the key.
+     */
+    public void insert(K key, V value) {
+        System.out.println("Inserting " + key);
 
-		ByteBuffer buffer1 = ByteBuffer.allocate(512);
-		ByteBuffer buffer2 = ByteBuffer.allocate(512);
+        BPNode<K, V> insertPlace = find(nodeFactory.getNode(rootNumber), key);
 
-		node1.save(buffer1);
-		node2.save(buffer2);
+        // TODO ...
 
-		BPNode<String, Integer> newNode1 = new BPNode<>(false);
-		BPNode<String, Integer> newNode2 = new BPNode<>(true);
+        // Need to call insertOnParent after performing a leaf node split
+    }
 
-		newNode1.load(buffer1, k -> k, s -> Integer.parseInt(s));
-		newNode2.load(buffer2, k -> k, s -> Integer.parseInt(s));
+    /**
+     * Insert on parent node a divider key after a child has been splitted.
+     *
+     * @param left  Left B+Tree node after a split has been made.
+     * @param key   Divider key (according to the description in {@link SplitResult}.
+     * @param right Right B+Tree node after a split has been made.
+     */
+    private void insertOnParent(BPNode<K, V> left, K key, BPNode<K, V> right) {
+        // TODO ...
 
-		System.out.println("Original root: " + node1 + ", parent = " + node1.parent + ", next = " + node1.next + ", number = " + newNode1.number);
-		System.out.println("Loaded root:   " + newNode1 + ", parent = " + newNode1.parent + ", next = " + newNode1.next + ", number = " + newNode1.number);
+        // Need to keep calling insertOnParent after performing an internal node split
+    }
 
-		System.out.println("Original leaf: " + node2 + ", parent = " + node2.parent + ", next = " + node2.next + ", number = " + newNode2.number);
-		System.out.println("Loaded leaf:   " + newNode2 + ", parent = " + newNode2.parent + ", next = " + newNode2.next + ", number = " + newNode2.number);
-	}
+    /**
+     * Returns a value associated with a particular key.
+     *
+     * @param key The key.
+     * @return The value associated with the provided key.
+     */
+    public V get(K key) {
+        BPNode<K, V> node = find(nodeFactory.getNode(rootNumber), key);
+        // Assuming the node contains keys and values in sorted order
+        for (int i = 0; i < node.getNumberOfKeys(); i++) {
+            if (equal(node.getKey(i), key)) {
+                return node.getValue(i);
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns the leaf node where we should look for the provided key.
+     *
+     * @param node The current node in the recursive search procedure.
+     * @param key  The key being searched.
+     * @return The leaf node where we should look for the provided key.
+     */
+    private BPNode<K, V> find(BPNode<K, V> node, K key) {
+
+        if (node.isLeaf()) {
+            return node;
+        }
+        // Find the child to descend into
+        for (int i = 0; i < node.getNumberOfKeys(); i++) {
+            if (more(key, node.getKey(i))) {
+                continue;
+            } else if (equal(key, node.getKey(i))) {
+                return find(nodeFactory.getNode(node.getChild(i + 1)), key);
+            } else {
+                return find(nodeFactory.getNode(node.getChild(i)), key);
+            }
+        }
+        // If key is greater than all keys in the node, go to the last child
+        return find(nodeFactory.getNode(node.getChild(node.getNumberOfKeys())), key);
+    }
 }
